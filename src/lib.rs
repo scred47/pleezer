bbf0bab103976a8350081706f0e1200814430ee1
Roderick van Domburg <roderick@vandomburg.net>
2024-07-31 22:37:06 +0200

    Silence documentation lints for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-//! TODO: add docs
+//! TODO: add top-level docs
 #![deny(clippy::all)]
 #![warn(clippy::pedantic)]
+// TODO : add documentation
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::missing_panics_doc)]
 
 #[macro_use]
 extern crate log;
