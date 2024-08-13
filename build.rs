4f8b0d2fba0f6521a74813b583d8d144e25d2aee
Roderick van Domburg <roderick@vandomburg.net>
2024-08-13 14:17:49 +0200

    Vendored protoc binary (#6)
    - Add protoc-bin-vendored crate
    - Use vendored protoc binary to generate protobufs

--- build.rs
+++ build.rs
@@ -3,8 +3,9 @@ use std::path::Path;
 fn main() {
     let proto_dir = Path::new("src/protocol/connect/protos");
 
-    // Use `protoc` if available or fall back to a pure Rust parser.
     protobuf_codegen::Codegen::new()
+        .protoc()
+        .protoc_path(&protoc_bin_vendored::protoc_bin_path().expect("could not find protoc binary"))
         .cargo_out_dir("protos")
         .include(proto_dir)
         .input(proto_dir.join("queue.proto"))
